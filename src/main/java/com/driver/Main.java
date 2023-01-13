f494667e50f5d19f8079170d2d63ad5b7b006a45
Satish <satishkrgupta555@gmail.com>
2023-01-13 22:16:32 +0530

    Polymorphism v1.0

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,44 @@
 package com.driver;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
 
 public class Main {
+//    System.out.println("This is it");
+
+    public static class Product{
+        int x;
+        int y;
+
+        public Product(){
+            System.out.println("Constructor initiated");
+        }
+//        public void Product(int x, int y){
+//            this.x = x;
+//            this.y = y;
+//        }
+
+        public static int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+//            System.out.println("Double"+ x*y);
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Product p = new Product();
+        p.product(2, 8);
+//        System.out.println(p.product(2,4));
+
+        p.product(2, 5, 6);
+        p.product(2.0, 3.4);
+    }
 
 }
